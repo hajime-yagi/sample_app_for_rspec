d4ed07f232aa6de9d8448377a30db6f93b6a496f
hajime <touken1996@yahoo.co.jp>
2021-01-29 16:38:41 +0900

    factories作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title, "title_1")
+    content  { "テストです" }
+    status   { :Todo }
+    deadline  { 1.day.from_now }
+    association :user
   end
 end
