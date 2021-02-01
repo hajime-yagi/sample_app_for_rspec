e24ea7244879c4f8b29c24d56c5957039f720224
hajime <touken1996@yahoo.co.jp>
2021-02-01 17:34:10 +0900

    テストケース記入

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+describe 'ログイン前' do 
+  describe 'ページ遷移確認' do
+    context '新規登録画面にアクセス' do
+  it '新規登録ページのアクセスが失敗すること' do
+ end
 end
+   context 'タスク編集ページにアクセス' do
+   it '編集ページへのアクセスが失敗すること' do
+   end
+  end
+  context 'タスク詳細ページにアクセス' do
+    it '詳細ページへのアクセスに成功すること' do
+    end
+   end
+   context 'タスク一覧ページにアクセス' do
+    it '一覧ページへのアクセスに成功すること' do
+    end
+   end
+  end 
+end
+
+
+describe 'ログイン後' do
+  
+  describe 'タスク新規登録' do
+    context '入力が正常' do
+      it 'タスク新規登録が成功すること' do
+      end
+    end
+    context '登録済みタスクタイトルを入力' do
+      it 'タスク登録に失敗すること' do
+      end
+     end
+
+
+describe 'タスク編集' do
+     context '入力が正常' do
+      it 'タスク編集に成功すること' do
+      end
+    end
+    context 'タイトル未入力' do
+      it 'タスク編集に失敗すること' do
+      end
+    end
+    context '登録済みタスクタイトルを入力' do
+      it 'タスク編集に失敗すること' do
+      end
+     end
+  
+describe 'タスク削除' do
+  it 'タスク削除に成功すること' do
+  end
+end
+
+
+
+    
