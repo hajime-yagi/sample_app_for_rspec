5dacefc656a4452f1138a333450c6e0c53b06be4
hajime <touken1996@yahoo.co.jp>
2021-02-02 20:20:40 +0900

    tasks_test finished

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -68,32 +68,64 @@ describe 'ログイン後' do
         visit new_task_path
         fill_in 'Title',with: task.title
         fill_in 'Content', with: '内容'
-        select 'todo',from: status
-
+        click_button 'Create Task'
+        expect(page).to have_content '1 error prohibited this task from being saved:'
+        expect(page).to have_content 'Title has already been taken'
+        expect(current_path).to eq tasks_path
       end
      end
-
+    end
 
 describe 'タスク編集' do
+  let!(:task) { create(:task, user: user)}
+  let(:another_task){ create(:task, user: user)}
      context '入力が正常' do
       it 'タスク編集に成功すること' do
+        visit edit_task_path(task)
+        fill_in 'Title', with: 'タイトル'
+        fill_in 'Content', with: '内容'
+        click_button 'Update Task'
+        expect(page).to have_content 'Task was successfully updated.'
+        expect(page).to have_content 'タイトル'
+        expect(page).to have_content '内容'
+        expect(current_path).to eq task_path(task)
       end
     end
     context 'タイトル未入力' do
       it 'タスク編集に失敗すること' do
+        visit edit_task_path(task)
+        fill_in 'Title', with: ''
+        fill_in 'Content', with: '内容'
+        click_button 'Update Task'
+        expect(page).to have_content '1 error prohibited this task from being saved:'
+        expect(page).to have_content "Title can't be blank"
+        expect(current_path).to eq task_path(task)
       end
     end
     context '登録済みタスクタイトルを入力' do
       it 'タスク編集に失敗すること' do
+        visit edit_task_path(task)
+        fill_in 'Title',with: another_task.title
+        fill_in 'Content',with: '内容'
+        click_button 'Update Task'
+        expect(page).to have_content '1 error prohibited this task from being saved:'
+        expect(page).to have_content 'Title has already been taken'
+        expect(current_path).to eq task_path(task)
       end
      end
   
 describe 'タスク削除' do
     it 'タスク削除に成功すること' do
+      visit root_path
+      click_link 'Destroy'
+      expect(page.accept_confirm).to eq 'Are you sure?'
+      expect(page).to have_content 'Task was successfully destroyed.'
+      expect(current_path).to eq tasks_path
+      expect(page).to_not have_content task.title
+
     end
    end
   end
-end
 end
 end
 
